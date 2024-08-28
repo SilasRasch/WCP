df9e9d7f044326517afaf3ef85dfecfef67b21bf
Silas H. Rasch <silasrasch@gmail.com>
2024-08-28 22:36:25 +0200

    New DbContext + app settings

---
+++ WCP/Models/WcpDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using WCPShared.Models.BrandModels;
+using WCPShared.Models.OrderModels;
+using WCPShared.Models.UserModels;
+using WCPShared.Models.UserModels.CreatorModels;
+
+namespace WCPShared.Models
+{
+    public class WcpDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Creator> Creators { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        private readonly IConfiguration _configuration;
+
+        public WcpDbContext(DbContextOptions<WcpDbContext> options, IConfiguration config) : base(options)
+        {
+            _configuration = config;
+        }
+    }
+}
