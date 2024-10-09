9672cb6ea05d031af647f9ec00b6108a08f32648
Silas H. Rasch <silasrasch@gmail.com>
2024-10-09 20:42:39 +0200

    Project dashboard

--- WCPAdminFrontEnd/Program.cs
+++ WCPAdminFrontEnd/Program.cs
@@ -1,11 +1,23 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using SlackNet.AspNetCore;
 using WCPAdminFrontEnd.Components;
+using WCPShared.Extensions;
+using WCPShared.Services;
+using WCPShared.Services.StaticHelpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.ConfigureDbContext(builder.Configuration);
+builder.Services.AddDataServices();
+builder.Services.AddScoped<SlackNotificationService>();
+builder.Services.AddSlackNet(options =>
+{
+    options.UseApiToken(Secrets.GetSlackKey(builder.Configuration));
+});
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
