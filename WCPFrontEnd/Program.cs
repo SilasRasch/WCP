e92cb6c7017b9e38597f54b3aa90d77377f78f34
Silas H. Rasch <silasrasch@gmail.com>
2024-10-11 00:11:20 +0200

    I tried

---
+++ WCPFrontEnd/Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using SlackNet.AspNetCore;
+using WCPShared.Extensions;
+using WCPShared.Services;
+using WCPShared.Services.StaticHelpers;
+using MudBlazor.Services;
+using WCPFrontEnd.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
+
+builder.Services.ConfigureDbContext(builder.Configuration);
+builder.Services.AddDataServices();
+builder.Services.AddScoped<SlackNotificationService>();
+builder.Services.AddSlackNet(options =>
+{
+    options.UseApiToken(Secrets.GetSlackKey(builder.Configuration));
+});
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.Name = "auth_token";
+        options.LoginPath = "/login";
+        options.Cookie.MaxAge = TimeSpan.FromMinutes(30);
+        options.AccessDeniedPath = "/access-denied";
+    });
+builder.Services.AddAuthorizationCore();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
